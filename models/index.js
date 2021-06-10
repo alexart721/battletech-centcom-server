ac5511abc72f3456e099055080825ff72939dbc9
Alex Teodorescu <ateodorescu721@gmail.com>
2021-06-10 14:47:12 -0400

    uncomment heroku dotenv for dev work

--- models/index.js
+++ models/index.js
@@ -1,10 +1,9 @@
-// const path = require('path'); // Comment out for Heroku commits
-// require('dotenv').config({ path: path.join(__dirname, '..', '/.env') }); // Comment out for Heroku commits
+const path = require('path'); // Comment out for Heroku commits
+require('dotenv').config({ path: path.join(__dirname, '..', '/.env') }); // Comment out for Heroku commits
 const { DATABASE_URL, DB } = process.env;
 const { Sequelize } = require('sequelize');
 
-// const sequelize = new Sequelize(`${DATABASE_URL}`); // Comment out for Heroku commits
-const sequelize = new Sequelize(`${DATABASE_URL}`, { ssl: { rejectUnauthorized: false } }); // Use for Heroku commits
+const sequelize = new Sequelize(`${DATABASE_URL}`);
 
 sequelize.authenticate().then(res => {
   console.log(`Connection established to ${DB}!`);
