7ad64bb7525a3cba445c0aeb26256440c8cd414e
Alex Teodorescu <ateodorescu721@gmail.com>
2021-06-10 14:14:59 -0400

    add DB dotenv variable

--- models/index.js
+++ models/index.js
@@ -1,15 +1,15 @@
 // const path = require('path'); // Comment out for Heroku commits
 // require('dotenv').config({ path: path.join(__dirname, '..', '/.env') }); // Comment out for Heroku commits
-const { DATABASE_URL } = process.env;
+const { DATABASE_URL, DB } = process.env;
 const { Sequelize } = require('sequelize');
 
 // const sequelize = new Sequelize(`${DATABASE_URL}`); // Comment out for Heroku commits
 const sequelize = new Sequelize(`${DATABASE_URL}`, { ssl: { rejectUnauthorized: false } }); // Use for Heroku commits
 
 sequelize.authenticate().then(res => {
-  console.log('Connection established to btcentcomdb!');
+  console.log(`Connection established to ${DB}!`);
 }).catch(err => {
-  console.error('Unable to connect to btcentcomdb, error: ', err);
+  console.error(`Unable to connect to ${DB}, error: `, err);
 });
 
 module.exports = sequelize;
