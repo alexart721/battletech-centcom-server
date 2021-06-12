793c0335c743ebad4aa9c19f685b0b23770fec81
Alex Teodorescu <ateodorescu721@gmail.com>
2021-06-12 11:48:40 -0400

    updated ssl connection options for Heroku

--- models/index.js
+++ models/index.js
@@ -2,7 +2,14 @@ const { DATABASE_URL, DB } = process.env;
 const { Sequelize } = require('sequelize');
 
 // const sequelize = new Sequelize(`${DATABASE_URL}`); // For local testing
-const sequelize = new Sequelize(`${DATABASE_URL}`, { dialectOptions: { ssl: true } }); // For Heroku deployment
+const sequelize = new Sequelize(`${DATABASE_URL}`, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+}); // For Heroku deployment
 
 sequelize.authenticate().then(res => {
   console.log(`Connection established to ${DB}!`);
